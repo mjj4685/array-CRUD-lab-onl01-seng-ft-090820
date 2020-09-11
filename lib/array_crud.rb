156e2a96fc9023bed95e9a06f50f49de7274b3c4
MJ Johnson <mjohnson4685@gmail.com>
2020-09-11 20:00:34 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-create_an_array=["green","rioobos","black","arrays!"]
+create_an_array=["wow","rioobos","black","arrays!"]
 end
 
 def add_element_to_end_of_array(array, element)
